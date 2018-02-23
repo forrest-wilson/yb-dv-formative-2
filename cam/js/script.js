1b715477ba784041aa8ce407280653d33a52aaae
Cameron mitchell Shaw <845872712@bun2-206-14.local>
2018-02-23 15:50:15 +1300

    updates on ajax

--- cam/js/script.js
+++ cam/js/script.js
@@ -1,1 +1,21 @@
 console.log('i am working');
+
+var ClientId = '2Osl6xc1DVNMd4BRRioCCOgITqZqSLOMimmcwUdo';
+
+$.ajax({
+	url: 'https://images-api.nasa.gov/search?q=apollo&media_type=image',
+	dataType: 'json',
+	success: function(nasaData) {
+		if (nasaData) {
+			console.log(nasaData.collection.items);
+			var imgList = "";
+		    $.each(nasaData.collection.items, function () {
+      			imgList += '<li><img src= "' + this.links["0"].href + '"></li>';
+    		});
+   			$('#wrapper').append(imgList);
+		}
+	},
+	error: function(error) {
+		console.log('error');
+	}
+})
