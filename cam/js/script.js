0cd818c1b69be620726ff49a75f3db6fc1d260a9
Cameron mitchell Shaw <845872712@bun2-206-14.local>
2018-02-26 15:50:46 +1300

    api integration

--- cam/js/script.js
+++ cam/js/script.js
@@ -2,20 +2,32 @@ console.log('i am working');
 
 var ClientId = '2Osl6xc1DVNMd4BRRioCCOgITqZqSLOMimmcwUdo';
 
-$.ajax({
-	url: 'https://images-api.nasa.gov/search?q=apollo&media_type=image',
-	dataType: 'json',
-	success: function(nasaData) {
-		if (nasaData) {
-			console.log(nasaData.collection.items);
-			var imgList = "";
-		    $.each(nasaData.collection.items, function () {
-      			imgList += '<li><img src= "' + this.links["0"].href + '"></li>';
-    		});
-   			$('#wrapper').append(imgList);
-		}
-	},
-	error: function(error) {
-		console.log('error');
-	}
+var searchInputValue = document.getElementById('search');
+console.log(searchInputValue);
+// var userQuery = 
+
+
+
+
+
+$('#formSearch').submit(function(){ 
+	event.preventDefault();
+	console.log(searchInputValue);
+		$.ajax({
+			url: 'https://images-api.nasa.gov/search?q=+userQuery+&media_type=image',
+			dataType: 'json',
+			success: function(nasaData) {
+				if (nasaData) {
+					console.log(nasaData.collection.items);
+					var imgList = "";
+				    $.each(nasaData.collection.items, function () {
+		      			imgList += '<li><img src= "' + this.links['0'].href + '"></li>';
+		    		});
+		   			$('#wrapper').append(imgList);
+				}
+			},
+			error: function(error) {
+				console.log('error');
+			}
+		})
 })
