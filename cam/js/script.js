160d1dc3e73464473ad7164474858ce443d19a71
Cameron mitchell Shaw <845872712@bun2-206-14.local>
2018-02-27 14:49:06 +1300

    validation

--- cam/js/script.js
+++ cam/js/script.js
@@ -2,13 +2,13 @@
 
 var ClientId 			= '2Osl6xc1DVNMd4BRRioCCOgITqZqSLOMimmcwUdo';
 var searchInputValue	= document.getElementById('text');
+var emptyDiv 			= document.getElementById('noResult');
 var api 				= 'https://images-api.nasa.gov/search?q=';
 var units				= '&media_type=image';
 
-
+console.dir(emptyDiv);
 $('#search').click(function(){ 
 	event.preventDefault();
-	console.log(searchInputValue.value);
 	$('#wrapper').children('li').remove();
 		$.ajax({
 			url: api + searchInputValue.value + units,
@@ -24,6 +24,7 @@ $('#search').click(function(){
 				}
 				if (searchInputValue.value == '') {
 					$('#wrapper').children("li").remove();
+					emptyDiv.innerHTML = "No results found, try again";
 				}
 			},
 			error: function(error) {
